44184d352e66ba4f65185ab691b85f1b278c03de
Adam Harris <aharris88@users.noreply.github.com>
2014-05-27 14:55:08 -0600

    Add getLetter function

--- board.js
+++ board.js
@@ -38,6 +38,28 @@ var board = function() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
+  var getLetter = function(die, side) {\
+    var dice = [
+      ['A', 'A', 'E', 'E', 'G', 'N'],
+      ['E', 'L', 'R', 'T', 'T', 'Y'],
+      ['A', 'O', 'O', 'T', 'T', 'W'],
+      ['A', 'B', 'B', 'J', 'O', 'O'],
+      ['E', 'H', 'R', 'T', 'V', 'W'],
+      ['C', 'I', 'M', 'O', 'T', 'U'],
+      ['D', 'I', 'S', 'T', 'T', 'Y'],
+      ['E', 'I', 'O', 'S', 'S', 'T'],
+      ['D', 'E', 'L', 'R', 'V', 'Y'],
+      ['A', 'C', 'H', 'O', 'P', 'S'],
+      ['H', 'I', 'M', 'N', 'Qu','U'],
+      ['E', 'E', 'I', 'N', 'S', 'U'],
+      ['E', 'E', 'G', 'H', 'N', 'W'],
+      ['A', 'F', 'F', 'K', 'P', 'S'],
+      ['H', 'L', 'N', 'N', 'R', 'Z'],
+      ['D', 'E', 'I', 'L', 'R', 'X']
+    ];
+    return dice[die][side];
+  };
+
   var drawBoard = function() {
     for(var i = 0; i < tileLocations.length; i++) {
       for(var j = 0; j < tileLocations[i].length; j++) {
